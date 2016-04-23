1c77345f37dea55aa660c3932733711d7b4d20ec
adam-power <adampower1a7ca@gmail.com>
2016-04-23 15:33:59 -0400

    Added autoincrement to primary keys

--- db/dbcreate.js
+++ db/dbcreate.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database(__dirname + '/inspections.db');
 
 db.run("CREATE TABLE IF NOT EXISTS Establishment( \
-			eid		INT NOT NULL, \
+			eid		INT NOT NULL AUTOINCREMENT, \
 			name	TEXT NOT NULL, \
 			pic		TEXT, \
 			addr1	TEXT, \
@@ -17,14 +17,14 @@ db.run("CREATE TABLE IF NOT EXISTS Establishment( \
 			PRIMARY KEY(eid) )");
 
 db.run("CREATE TABLE IF NOT EXISTS Form( \
-			fid		INT NOT NULL, \
+			fid		INT NOT NULL AUTOINCREMENT, \
 			eid		INT NOT NULL, \
 			date 	TEXT NOT NULL, \
 			PRIMARY KEY(fid), \
 			FOREIGN KEY(eid) REFERENCES Establishment(eid) )");
 
 db.run("CREATE TABLE IF NOT EXISTS Violation( \
-			vid		INT NOT NULL, \
+			vid		INT NOT NULL AUTOINCREMENT, \
 			fid		INT NOT NULL, \
 			codeRef	TEXT, \
 			isCrit	BOOLEAN NOT NULL, \
