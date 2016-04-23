ea4c3eab56276bbe701827c8961e91d72fe4b159
Anthony Li <anthli1895@gmail.com>
2016-04-23 10:33:10 -0400

    Added search functionality to the forms page

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,13 @@
-// Ionic Starter App
+var app = angular.module('inspectorGadget', [
+  'ionic',
+  'inspectorGadget.controllers',
+  'inspectorGadget.services'
+]);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar
+    // above the keyboard for form inputs)
     if (window.cordova && window.cordova.plugins &&
       window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -22,24 +19,23 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function($stateProvider, $urlRouterProvider) {
+// Place the nav-bar on the bottom of the screen for Android
+app.config(['$ionicConfigProvider', function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
+}]);
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-  .state('page', {
+// Ionic uses AngularUI Router which uses the concept of states
+// Learn more here: https://github.com/angular-ui/ui-router
+// Set up the various states which the app can be in.
+// Each state's controller can be found in controllers.js
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('page', {
     url: '/page',
     abstract: true,
     templateUrl: 'templates/pages.html'
   })
-
-  // Each tab has its own nav history stack:
 
   .state('page.dash', {
     url: '/dash',
@@ -57,6 +53,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       'page-newinspection': {
         templateUrl: 'templates/newinspection.html',
         controller: 'NewInspectionCtrl'
+      }
+    }
+  })
+  .state('page.addviolation', {
+    url: '/addviolation',
+    views: {
+      'page-addviolation': {
+        templateUrl: 'templates/addviolation.html',
+        controller: 'AddViolationCtrl'
       }
     }
   })
@@ -78,19 +83,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'FormViewerCtrl'
       }
     }
-  })
-
-  .state('page.addviolation', {
-    url: '/addviolation',
-    views: {
-      'page-addviolation': {
-        templateUrl: 'templates/addviolation.html',
-        controller: 'AddViolationCtrl'
-      }
-    }
   });
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/page/dash');
-
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,31 @@
-angular.module('starter.services', [])
+var app = angular.module('inspectorGadget.services', []);
 
-.factory('Forms', function() {
+app.factory('Violations', function() {
+  var violations = [
+    {id: 1, name: "Violation 1"},
+    {id: 2, name: "Violation 2"},
+    {id: 3, name: "Violation 3"},
+    {id: 4, name: "Violation 4"},
+    {id: 5, name: "Violation 5"}
+  ];
+
+  return {
+    all: function() {
+      return violations;
+    },
+    get: function(formPath) {
+      for (var i = 0; i < violations.length; i++) {
+        if (violations[i].id === violationID) {
+          return forms[i];
+        }
+      }
+
+      return null;
+    }
+  };
+});
+
+app.factory('Forms', function() {
   var forms = [{
     path: "1999_fda_food_code.pdf",
     name: "1999 FDA Food Code"
